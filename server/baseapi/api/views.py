68276fa0913e908736352544fe7a857f2a081616
ahmelevs <andrejs.hmelovs@gmail.com>
2024-10-11 16:43:43 +0400

    create book url

--- server/baseapi/api/views.py
+++ server/baseapi/api/views.py
@@ -11,3 +11,11 @@ def get_books(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def create_book(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
